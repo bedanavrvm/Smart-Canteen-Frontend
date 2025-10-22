9d954cb5671548f31c023d6286ad4808e1bf9bd0
vaulthunterz <4aulthunterz@proton.me>
2025-10-22 18:35:14 +0300

    Updated base url to point to the deployed backend on pythonAnywhere

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { refreshToken } from "./auth";
 
 const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://bedanaurum.pythonanywhere.com/api",
   headers: {
     "Content-Type": "application/json",
   },
